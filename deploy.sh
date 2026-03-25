c3656ee24b6935fc63cb97da7e89aa57c8a219ef
Minoru Yamada <minorugh@gmail.com>
2026-03-25 19:32:49 +0900

    Remove unused deploy.sh (replaced by makefile)

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-git add . && git diff --cached --exit-code --quiet && echo "\nnothing to commit, working tree clean!"|| \
-git commit -a -m "Updated: `date +'%Y-%m-%d %H:%M:%S'`" && \
-git push origin main
